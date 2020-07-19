9d228cd641ede7ef2a493785d9f5c1b533376892
The Hatter <quazariun@gmail.com>
2020-07-19 11:25:44 -0300

    Add exercises from Exercism and Reorganize folders

---
+++ exercism/ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,6 @@
+module ResistorColorDuo
+  def self.value(input)
+    colors = { "black" => "0", "brown" => "1", "red" => "2", "orange" => "3", "yellow" => "4", "green" => "5", "blue" => "6", "violet" => "7", "grey" => "8", "white" => "9"}
+    (colors[input[0]] + colors[input[1]]).to_i
+  end
+end
